87219ae05d91d77e0294cc855f12ad700e409567
Sebziel <sebzie1996@gmail.com>
2022-05-07 23:30:08 +0200

    Adding newfeatures

--- SimpleApp.py
+++ SimpleApp.py
@@ -1,17 +1,20 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from model import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return render_template("welcome.html", message='Message from view!')
+    return render_template("welcome.html", cards=db)
 
-@app.route("/card")
-def card_view():
-    card = db[1]
-    return render_template("card.html", card=card)
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", card=card, index=index, max_index=len(db)-1)
+    except IndexError:
+        abort(404)
 
 @app.route("/FirstPage")
 def FirstPage():
