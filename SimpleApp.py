f0cb4a0a504fa413f162dc5edf140b8ef0061dee
Sebziel <sebzie1996@gmail.com>
2022-05-19 22:02:51 +0200

    added logging

--- SimpleApp.py
+++ SimpleApp.py
@@ -2,23 +2,29 @@ from datetime import datetime
 from flask import Flask, render_template, abort
 from model import db , get_counter_value , update_counter
 import FlowersUtilities
+import logging
+
 
 app = Flask(__name__)
+logging.basicConfig(filename="/home/sebastian/Desktop/Projects/FunkyFlaskAppLogs/record.log", level=logging.DEBUG, format=f'%(asctime)s %(levelname)s : %(message)s')
 
-## Todo: welcome_counter = 0
 
 @app.route("/")
 def welcome():
     update_counter()
+    app.logger.debug('Serving welcome page')
     current_time = str(datetime.now())
     counter = get_counter_value()
+    app.logger.debug("Setting counter from welcome page to: " + str(counter))
     allFlowersDict = FlowersUtilities.get_flowers_data()
+    app.logger.debug("gathering data from Flowers.json:" + str(allFlowersDict))
     allFlowers = allFlowersDict[0]
     return render_template("welcome.html", cards=db, current_time = current_time, counter = counter , allFlowers = allFlowers)
 
 @app.route("/card/<int:index>")
 def card_view(index):
     update_counter()
+    app.logger.debug('Serving specific card page')
     try:
         card = db[index]
         return render_template("card.html", card=card, index=index, max_index=len(db)-1)
@@ -41,17 +47,20 @@ def add_card():
 @app.route("/FirstPage")
 def FirstPage():
     update_counter()
+    app.logger.debug("Serving 'FirstPage' page")
     return "There's First page it was served at " +str(datetime.now())
 
 
 @app.route("/ViewCounter")
 def ViewCounter():
+    app.logger.debug('Serving viewcounter pagge')
     update_counter()
     return str(get_counter_value())
 
 @app.route("/flowerList")
 def flower_list_view():
     update_counter()
+    app.logger.debug("Serving flowerList page")
     try:
         flowerlist = FlowersUtilities.get_flowers_list()
         return render_template("flowers.html", flowerlist=flowerlist)
@@ -63,6 +72,7 @@ def flower_list_view():
 @app.route("/flower/<string:dindex>")
 def flower_view(dindex):
     update_counter()
+    app.logger.debug("Serving: " + str(dindex) + " flower page")
     try:
         specificFlower = FlowersUtilities.get_flowers_data()
         flowerlist = specificFlower[0] #Wyciaga zerowy obiekt ze wzgledu na strukture danych w Jsonie
